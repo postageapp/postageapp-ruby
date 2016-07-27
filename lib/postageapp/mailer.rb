dfcc7dd4c6394d8e13e14c0f3df74248ac1ec27a
Scott Tadman <tadman@postageapp.com>
2016-07-27 14:47:42 -0400

    Setting the mailer_4 as default

--- lib/postageapp/mailer.rb
+++ lib/postageapp/mailer.rb
@@ -11,11 +11,11 @@ if (defined?(ActionMailer))
   # currently installed on the system. Assuming we're dealing only with
   # ones that come with Rails 2 and 3
   case (ActionMailer::VERSION::MAJOR)
-  when 4,5
-    require File.expand_path('mailer/mailer_4', File.dirname(__FILE__))
   when 3
     require File.expand_path('mailer/mailer_3', File.dirname(__FILE__))
-  else
+  when 2
     require File.expand_path('mailer/mailer_2', File.dirname(__FILE__))
+  else
+    require File.expand_path('mailer/mailer_4', File.dirname(__FILE__))
   end
 end
