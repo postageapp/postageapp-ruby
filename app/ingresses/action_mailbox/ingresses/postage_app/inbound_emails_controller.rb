693b655036c3800238fd52b046e6404b4a5c41ed
Scott Tadman <tadman@postageapp.com>
2019-05-07 14:21:54 -0400

    Minimization of HMAC validation method

--- app/ingresses/action_mailbox/ingresses/postage_app/inbound_emails_controller.rb
+++ app/ingresses/action_mailbox/ingresses/postage_app/inbound_emails_controller.rb
@@ -29,25 +29,22 @@ private
     if (PostageApp.config.postback_secret.present?)
       ActiveSupport::SecurityUtils.secure_compare(
         request.headers["X-PostageApp-Signature"],
-        hmac_signature(
-          message: message_param,
-          secret: PostageApp.config.postback_secret
-        )
+        hmac_signature(message_param, PostageApp.config.postback_secret)
       )
     else
       raise ArgumentError, <<~END.squish
-        Missing required PostageApp postback secret which can be set as
-        postageapp: postback_secret: in the Rails Encypted Credentials, as
-        POSTAGEAPP_API_POSTBACK_SECRET in the environment, or via a
-        config/initializer script using the PostageApp.config method.
+        Missing required PostageApp "postback secret" which can be set as
+        in the Rails Encypted Credentials, as POSTAGEAPP_API_POSTBACK_SECRET
+        in the environment, or via a config/initializer script using the
+        PostageApp.config method.
       END
     end
   end
 
 private
-  def hmac_signature(message:, secret:)
+  def hmac_signature(*content)
     Base64.strict_encode64(
-      OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, secret, message)
+      OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, *content)
     )
   end
 end
