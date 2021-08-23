1d305d2ce83a3aff53f80a2c6b569c4cb9da38f1
Scott Tadman <tadman@postageapp.com>
2021-08-23 12:54:13 -0400

    Restricting exposure of ActionMailbox

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PostageApp::Engine.routes.draw do
-  scope "/rails/action_mailbox", module: "action_mailbox/ingresses" do
-    post "/postageapp/inbound_emails" => "postage_app/inbound_emails#create",
-      as: :rails_postageapp_inbound_emails
+  if (defined?(ActionMailbox))
+    scope '/rails/action_mailbox', module: 'action_mailbox/ingresses' do
+      post '/postageapp/inbound_emails' => 'postage_app/inbound_emails#create',
+        as: :rails_postageapp_inbound_emails
+    end
   end
 end
